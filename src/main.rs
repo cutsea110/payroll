2da797c0e917eb08973ee323b3cc6b4524810dc2
cutsea110 <cutsea110@gmail.com>
2024-12-30 15:32:53 +0900

    refine

--- src/main.rs
+++ src/main.rs
@@ -23,30 +23,29 @@ trait ChgEmpTx {
 
 trait ChgEmpNameTx {
     fn new_name(&self) -> &str;
-
-    // NOTE: ChgEmpNameTx が ChgEmpTx を継承できたならこれは override になったはず
     fn change(&self, emp: &mut Emp) {
         emp.set_name(self.new_name());
     }
 }
-// ChgEmpName を使って ChgEmpNameTx を実装
-struct ChgEmpNameTxImpl {
-    name: String,
-}
-// ChgEmpNameTxImpl にはこれだけ実装して済ませたい (が、ChgEmpTx も実装しないといけない)
-impl ChgEmpNameTx for ChgEmpNameTxImpl {
-    fn new_name(&self) -> &str {
-        &self.name
-    }
-}
-
-// NOTE: ChgEmpNameTx が ChgEmpTx を継承できたならこれは書かなくて済んだはず
-impl ChgEmpTx for ChgEmpNameTxImpl {
+// T が ChgEmpNameTx なら常に ChgEmpTx でもある (blanket impl)
+impl<T> ChgEmpTx for T
+where
+    T: ChgEmpNameTx,
+{
     fn change(&self, emp: &mut Emp) {
         ChgEmpNameTx::change(self, emp);
     }
 }
 
+// ChgEmpName を使って ChgEmpNameTx を実装
+struct ChgEmpNameTxImpl {
+    name: String,
+}
+impl ChgEmpNameTx for ChgEmpNameTxImpl {
+    fn new_name(&self) -> &str {
+        &self.name
+    }
+}
 fn main() {
     let tx = ChgEmpNameTxImpl {
         name: "Doe".to_string(),
