90b266330d45b3c381f4877bcdb1483a9b0525cf
cutsea110 <cutsea110@gmail.com>
2025-01-15 21:18:54 +0900

    add log for main

--- payroll-app/src/main.rs
+++ payroll-app/src/main.rs
@@ -12,16 +12,17 @@ fn main() -> Result<(), anyhow::Error> {
 
     let db = HashDB::new();
     info!("DB initialized: {:?}", db);
-
-    // テストスクリプトを読み込んでシナリオを実行
-    let input = fs::read_to_string("script/test.scr")?;
     let tx_factory = TxFactoryImpl::new(db.clone());
+
+    let script_path = "script/test.scr";
+    info!("Reading script: {}", script_path);
+    let input = fs::read_to_string(script_path)?;
+    info!("Parsing script");
     let tx_source = TextParserTxSource::new(&input, tx_factory);
     let tx_app = TxApp::new(tx_source);
 
-    info!("TxApp starting");
+    info!("TxApp running");
     tx_app.run()?;
-    info!("TxApp finished");
 
     println!("{:#?}", db);
     info!("TxApp finished");
