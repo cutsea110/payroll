12e3e6bc059bcd1d6f2f359f1da815ec0828c448
cutsea110 <cutsea110@gmail.com>
2025-01-30 18:53:28 +0900

    impl Args

--- payroll-app/src/main.rs
+++ payroll-app/src/main.rs
@@ -17,32 +17,33 @@ struct Args {
     script_file: Option<String>,
     opts: Options,
 }
+impl Args {
+    fn parse_args() -> Result<Args, anyhow::Error> {
+        let args: Vec<String> = env::args().collect();
+        let program = args.get(0).expect("program name");
+        let mut opts = Options::new();
+        opts.optflag("h", "help", "Print this help menu");
+
+        let matches = match opts.parse(&args[1..]) {
+            Ok(m) => m,
+            Err(f) => {
+                error!("parse_args: error parsing options: {}", f);
+                return Err(anyhow::Error::msg(f.to_string()));
+            }
+        };
+
+        Ok(Args {
+            help: matches.opt_present("h"),
+            program: program.to_string(),
+            script_file: matches.free.get(0).cloned(),
+            opts,
+        })
+    }
+}
 
 fn print_usage(args: Args) {
     let brief = format!("Usage: {} [options] FILE", args.program);
     print!("{}", args.opts.usage(&brief));
-}
-
-fn parse_args() -> Result<Args, anyhow::Error> {
-    let args: Vec<String> = env::args().collect();
-    let program = args.get(0).expect("program name");
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "Print this help menu");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => {
-            error!("parse_args: error parsing options: {}", f);
-            return Err(anyhow::Error::msg(f.to_string()));
-        }
-    };
-
-    Ok(Args {
-        help: matches.opt_present("h"),
-        program: program.to_string(),
-        script_file: matches.free.get(0).cloned(),
-        opts,
-    })
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -50,7 +51,7 @@ fn main() -> Result<(), anyhow::Error> {
 
     info!("TxApp starting");
 
-    let args = parse_args()?;
+    let args = Args::parse_args()?;
     if args.help {
         debug!("main: help flag is set");
         print_usage(args);
