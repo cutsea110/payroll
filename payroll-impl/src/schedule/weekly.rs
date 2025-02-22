883791e8a17c253eec9125f34288212d09d697bd
cutsea110 <cutsea110@gmail.com>
2025-02-22 13:44:56 +0900

    add tests for WeeklySchedule

--- payroll-impl/src/schedule/weekly.rs
+++ payroll-impl/src/schedule/weekly.rs
@@ -13,3 +13,38 @@ impl PaymentSchedule for WeeklySchedule {
         pay_date.checked_sub_days(Days::new(6)).unwrap()..=pay_date
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::NaiveDate;
+
+    #[test]
+    fn test_is_pay_date() {
+        let ws = WeeklySchedule;
+        assert!(ws.is_pay_date(NaiveDate::from_ymd_opt(2025, 1, 10).unwrap()));
+        assert!(!ws.is_pay_date(NaiveDate::from_ymd_opt(2025, 1, 11).unwrap()));
+        assert!(ws.is_pay_date(NaiveDate::from_ymd_opt(2025, 1, 17).unwrap()));
+        assert!(!ws.is_pay_date(NaiveDate::from_ymd_opt(2025, 1, 23).unwrap()));
+        assert!(ws.is_pay_date(NaiveDate::from_ymd_opt(2025, 1, 24).unwrap()));
+    }
+
+    #[test]
+    fn test_pay_period() {
+        let ws = WeeklySchedule;
+        let pay_date = NaiveDate::from_ymd_opt(2025, 1, 10).unwrap();
+        let pay_period = ws.get_pay_period(pay_date);
+        assert_eq!(
+            pay_period,
+            NaiveDate::from_ymd_opt(2025, 1, 4).unwrap()
+                ..=NaiveDate::from_ymd_opt(2025, 1, 10).unwrap()
+        );
+        let pay_date = NaiveDate::from_ymd_opt(2025, 1, 24).unwrap();
+        let pay_period = ws.get_pay_period(pay_date);
+        assert_eq!(
+            pay_period,
+            NaiveDate::from_ymd_opt(2025, 1, 18).unwrap()
+                ..=NaiveDate::from_ymd_opt(2025, 1, 24).unwrap()
+        );
+    }
+}
