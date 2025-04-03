bad6c58593710bc988d3d446fd6a76cd47cd2714
cutsea110 <cutsea110@gmail.com>
2025-04-03 18:24:33 +0900

    TestRunner

--- payroll-test/src/main.rs
+++ payroll-test/src/main.rs
@@ -3,88 +3,112 @@ use serde_json;
 use std::{
     env, fs,
     io::{BufRead, BufReader, Write},
-    process::{Command, Stdio},
+    process::{Child, ChildStdin, ChildStdout, Command, ExitStatus, Stdio},
 };
 
 use test_runner::{Paycheck, Verify};
 
-const APP_PATH: &str = "./target/debug/payroll-app";
+pub struct TestRunner {
+    child: Child,
+    stdin: ChildStdin,
+    reader: BufReader<ChildStdout>,
+}
+impl TestRunner {
+    pub fn new(app_path: &str) -> Self {
+        trace!("spawn_target called");
+        let mut child = Command::new(app_path)
+            .arg("-qfs")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("start child process");
+        trace!("app started: PID={}", child.id());
 
-fn check(script_file_path: &str) {
-    eprint!("Running test: {}", script_file_path);
+        // pipe of in/out to payroll-app
+        let stdin = child.stdin.take().expect("open stdin");
+        trace!("stdin opened");
+        let stdout = child.stdout.take().expect("open stdout");
+        trace!("stdout opened");
+        let reader = BufReader::new(stdout);
 
-    // run up payroll-app
-    let mut child = Command::new(APP_PATH)
-        .arg("-qfs")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("start app");
-    trace!("app started: PID={}", child.id());
-
-    // pipe of in/out to payroll-app
-    let stdin = child.stdin.as_mut().expect("open stdin");
-    trace!("stdin opened");
-    let stdout = child.stdout.take().expect("open stdout");
-    trace!("stdout opened");
-    let mut reader = BufReader::new(stdout);
-
-    trace!("script file path: {}", script_file_path);
-    let text = fs::read_to_string(&script_file_path).expect("read script");
-    trace!("script:\n{}", text);
-
-    // do commands
-    for (i, line) in text.lines().enumerate().map(|(i, l)| (i + 1, l)) {
-        if Verify::is_verify(line) {
-            // Payday の標準出力をキャプチャ
-            let mut output_json = String::new();
-            reader
-                .read_line(&mut output_json)
-                .expect("read from stdout");
-            debug!("test <- app: {}", output_json);
-
-            // JSON の検証
-            let expect = match Verify::parse(i, &line) {
-                Ok(v) => v,
-                Err(e) => {
-                    eprintln!("{}", e);
-                    break;
-                }
-            };
-            let actual: Paycheck = serde_json::from_str(&output_json).expect("parse JSON");
-            debug!("expect: {:?}, actual: {:?}", expect, actual);
-            match expect {
-                Verify::GrossPay { emp_id, gross_pay } => {
-                    debug!("verify gross pay");
-                    assert_eq!(actual.emp_id, emp_id);
-                    assert_eq!(actual.gross_pay, gross_pay);
-                }
-                Verify::Deductions { emp_id, deductions } => {
-                    debug!("verify deductions");
-                    assert_eq!(actual.emp_id, emp_id);
-                    assert_eq!(actual.deductions, deductions);
-                }
-                Verify::NetPay { emp_id, net_pay } => {
-                    debug!("verify net pay");
-                    assert_eq!(actual.emp_id, emp_id);
-                    assert_eq!(actual.net_pay, net_pay);
-                }
-            }
-        } else {
-            debug!("test -> app: {}", line);
-            writeln!(stdin, "{}", line).expect("write to stdin");
-            let mut buff = String::new();
-            reader.read_line(&mut buff).expect("read echo back");
-            debug!("test <- app: {}", buff);
-            stdin.flush().expect("flush stdin");
+        TestRunner {
+            child,
+            stdin,
+            reader,
         }
     }
+    fn read_line(&mut self, buf: &mut String) {
+        self.reader.read_line(buf).expect("read line");
+        self.stdin.flush().expect("flush");
+        debug!("test <- app: {}", buf);
+    }
+    fn write_line(&mut self, line: &str) {
+        debug!("test -> app: {}", line);
+        writeln!(self.stdin, "{}", line).expect("write line");
+    }
+    fn assert(&self, actual: Paycheck, expect: Verify) {
+        match expect {
+            Verify::GrossPay { emp_id, gross_pay } => {
+                debug!("verify gross pay");
+                assert_eq!(actual.emp_id, emp_id);
+                assert_eq!(actual.gross_pay, gross_pay);
+            }
+            Verify::Deductions { emp_id, deductions } => {
+                debug!("verify deductions");
+                assert_eq!(actual.emp_id, emp_id);
+                assert_eq!(actual.deductions, deductions);
+            }
+            Verify::NetPay { emp_id, net_pay } => {
+                debug!("verify net pay");
+                assert_eq!(actual.emp_id, emp_id);
+                assert_eq!(actual.net_pay, net_pay);
+            }
+        }
+    }
+    pub fn run(&mut self, script_file_path: &str) {
+        trace!("script file path: {}", script_file_path);
+        let text = fs::read_to_string(&script_file_path).expect("read script file");
+        trace!("script:\n{}", text);
 
-    eprintln!(" ... PASS");
-    debug!("test succeeded:{}", script_file_path);
-    // 終了処理
-    let _ = child.wait().expect("wait on child process");
-    debug!("child process exited: {}", child.id());
+        // execute commands
+        for (i, line) in text.lines().enumerate().map(|(i, l)| (i + 1, l)) {
+            if Verify::is_verify(line) {
+                // Payday の標準出力をキャプチャ
+                let mut output_json = String::new();
+                self.read_line(&mut output_json);
+
+                // JSON の検証
+                let expect: Verify = match Verify::parse(i, line) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        break;
+                    }
+                };
+                let actual: Paycheck = serde_json::from_str(&output_json).expect("parse JSON");
+                debug!("expect: {:?}, actual: {:?}", expect, actual);
+                self.assert(actual, expect);
+            } else {
+                self.write_line(line);
+                let mut buff = String::new();
+                self.read_line(&mut buff);
+            }
+        }
+    }
+    pub fn shutdown(mut self) -> ExitStatus {
+        debug!("stdin closing ...");
+        // After all commands are executed, close the standard output
+        // This will cause the child process to receive EOF and exit
+        // If this is not done, the child process will not exit
+        drop(self.stdin);
+        debug!("stdin dropped");
+
+        // termination
+        let exit_code = self.child.wait().expect("wait for child process");
+        trace!("child process({}) exited: {}", self.child.id(), exit_code);
+
+        exit_code
+    }
 }
 
 fn main() {
@@ -95,10 +119,20 @@ fn main() {
 
     info!("main starting");
 
-    // open test script
+    // skip program name
     for script_file_path in env::args().skip(1) {
         debug!("script file path: {}", script_file_path);
-        check(&script_file_path);
+        // OPEN: eprint
+        eprint!("Running test {}\t ... ", script_file_path);
+
+        // run up payroll-app
+        let mut runner = TestRunner::new("./target/debug/payroll-app");
+        runner.run(&script_file_path);
+        runner.shutdown();
+
+        // CLOSE: eprintln
+        eprintln!("PASS");
+        debug!("test succeeded:{}", script_file_path);
     }
 
     info!("main finished");
