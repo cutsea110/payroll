bd084c7ca1fa5befd741c4893a88630f22a07768
cutsea110 <cutsea110@gmail.com>
2025-04-03 21:18:57 +0900

    env APP_PATH support

--- payroll-test/src/main.rs
+++ payroll-test/src/main.rs
@@ -3,33 +3,32 @@ use std::env;
 
 use test_runner::TestRunner;
 
-const APP_PATH: &str = "./target/debug/payroll-app";
-
 fn main() {
     env_logger::Builder::from_default_env()
         .format_source_path(true)
         .format_line_number(true)
         .init();
+    let app_path = env::var("APP_PATH").unwrap_or("./target/debug/payroll-app".to_string());
 
     info!("main starting");
 
     // skip program name
-    for script_file_path in env::args().skip(1) {
-        debug!("script file path: {}", script_file_path);
+    for fp in env::args().skip(1) {
+        debug!("script file path: {}", fp);
         // OPEN: eprint
-        eprint!("Running test {}\t ... ", script_file_path);
+        eprint!("Running test {}\t ... ", fp);
 
         // run up payroll-app
-        let runner = TestRunner::new(APP_PATH);
-        let pass = runner.run(&script_file_path);
+        let runner = TestRunner::new(&app_path);
+        let pass = runner.run(&fp);
 
         // CLOSE: eprintln
         if pass {
             eprintln!("PASS");
-            debug!("test passed: {}", script_file_path);
+            debug!("test passed: {}", fp);
         } else {
             eprintln!("FAIL");
-            debug!("test failed: {}", script_file_path);
+            debug!("test failed: {}", fp);
         }
     }
 
