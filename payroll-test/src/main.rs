a346ac8a7d13033311cb1d579ba46fc234356482
cutsea110 <cutsea110@gmail.com>
2025-04-25 07:56:30 +0900

    update comments

--- payroll-test/src/main.rs
+++ payroll-test/src/main.rs
@@ -8,11 +8,13 @@ fn main() {
         .format_source_path(true)
         .format_line_number(true)
         .init();
+
+    // Expect payroll-app is in the target/debug directory and that built before running this test
     let target_path = env::var("TARGET_PATH").unwrap_or("./target/debug/payroll-app".to_string());
 
     info!("main starting");
 
-    // skip program name
+    // This 1 means to skip program name
     for fp in env::args().skip(1) {
         info!("test: {}", fp);
         // OPEN: eprint
