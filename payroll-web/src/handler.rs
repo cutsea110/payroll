4731ad5b003e965c310a3bdd866614f79947d6bc
cutsea110 <cutsea110@gmail.com>
2025-06-03 07:17:00 +0900

    rearrange module to files

---
+++ payroll-web/src/handler.rs
@@ -0,0 +1,84 @@
+use hs_db::HashDB;
+use log::{debug, error, trace};
+use std::{io::prelude::*, net::TcpStream, str, sync::Arc};
+
+use crate::tx_app_builder::TxAppBuilder;
+
+pub trait Handler {
+    fn handle_connection(&self, stream: TcpStream);
+}
+
+#[derive(Debug, Clone)]
+pub struct TcpHandler {
+    builder: TxAppBuilder,
+}
+impl TcpHandler {
+    pub fn new(db: HashDB, quiet: bool, chronograph: bool) -> Self {
+        Self {
+            builder: TxAppBuilder::new(db, quiet, chronograph),
+        }
+    }
+}
+impl Handler for TcpHandler {
+    fn handle_connection(&self, mut stream: TcpStream) {
+        trace!("Handling connection from {}", stream.peer_addr().unwrap());
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).expect("read from stream");
+        // TODO: check Content-Length header for larger requests
+        let text = match str::from_utf8(&buffer) {
+            Ok(v) => v.trim_end_matches('\0'),
+            Err(e) => {
+                error!("Invalid UTF-8 sequence: {}", e);
+                return;
+            }
+        };
+        let mut split = text.splitn(2, "\r\n\r\n");
+
+        let header = split.next().unwrap_or_default();
+        debug!("Received header:\n{}", header);
+        let body = split.next().unwrap_or_default();
+        debug!("Received body:\n{}", body);
+
+        let mut tx_app = self.builder.build(body);
+        let response = match tx_app.run() {
+            Ok(_) => {
+                trace!("Transaction app ran successfully");
+                "HTTP/1.1 200 OK\r\n\r\n"
+            }
+            Err(e) => {
+                error!("Error running transaction app: {}", e);
+                "HTTP/1.1 500 Server Error\r\n\r\n"
+            }
+        };
+        trace!("sent response: {}", response);
+
+        stream.write(response.as_bytes()).expect("write to stream");
+        stream.flush().expect("flush stream");
+    }
+}
+
+#[derive(Clone)]
+pub struct ChronographHandler {
+    handler: Arc<dyn Handler + Send + Sync>,
+}
+impl ChronographHandler {
+    fn new(handler: Arc<dyn Handler + Send + Sync>) -> Self {
+        Self { handler }
+    }
+}
+impl Handler for ChronographHandler {
+    fn handle_connection(&self, stream: TcpStream) {
+        trace!("handle_connection called");
+        let start = std::time::Instant::now();
+
+        self.handler.handle_connection(stream);
+
+        let elapsed = start.elapsed();
+        debug!("handler elapsed: {:?}", elapsed);
+        println!("handler elapsed: {:?}", elapsed);
+    }
+}
+pub fn with_chronograph(handler: Arc<dyn Handler + Send + Sync>) -> Arc<dyn Handler + Send + Sync> {
+    trace!("with_chronograph called");
+    Arc::new(ChronographHandler::new(handler))
+}
---
+++ payroll-web/src/tx_app_builder.rs
@@ -0,0 +1,68 @@
+use log::{debug, trace};
+
+use app::Application;
+use hs_db::HashDB;
+use payroll_impl::PayrollFactoryImpl;
+use std::str;
+use text_parser_tx_source::TextParserTxSource;
+use tx_app::{Runner, TxApp, TxSource};
+use tx_app_impl::{app_impl, reader_impl, runner_impl};
+use tx_impl::TxFactoryImpl;
+
+#[derive(Debug, Clone)]
+pub struct TxAppBuilder {
+    db: HashDB,
+
+    quiet: bool,
+    chronograph: bool,
+}
+impl TxAppBuilder {
+    pub fn new(db: HashDB, quiet: bool, chronograph: bool) -> Self {
+        Self {
+            db,
+            quiet,
+            chronograph,
+        }
+    }
+
+    pub fn build(&self, request_body: &str) -> Box<dyn Application> {
+        trace!("build_tx_app called");
+
+        let tx_source = self.make_tx_source(request_body);
+        let tx_runner = self.make_tx_runner();
+
+        let mut tx_app: Box<dyn Application> = Box::new(TxApp::new(tx_source, tx_runner));
+        if self.chronograph {
+            debug!("Adding fail-open mode");
+            tx_app = app_impl::with_chronograph(tx_app);
+        }
+
+        tx_app
+    }
+    fn make_tx_runner(&self) -> Box<dyn Runner> {
+        trace!("make_tx_runner called");
+
+        let mut tx_runner = if self.quiet {
+            debug!("Quiet mode enabled");
+            runner_impl::silent_runner()
+        } else {
+            debug!("Echoback mode enabled");
+            runner_impl::echoback_runner()
+        };
+        if self.chronograph {
+            debug!("Chronograph mode enabled");
+            tx_runner = runner_impl::with_chronograph(tx_runner);
+        };
+
+        tx_runner
+    }
+    fn make_tx_source(&self, body: &str) -> Box<dyn TxSource> {
+        trace!("make_tx_source called");
+
+        let tx_factory = TxFactoryImpl::new(self.db.clone(), PayrollFactoryImpl);
+        let tx_source =
+            TextParserTxSource::new(tx_factory, reader_impl::string_reader(body.to_string()));
+
+        Box::new(tx_source)
+    }
+}
